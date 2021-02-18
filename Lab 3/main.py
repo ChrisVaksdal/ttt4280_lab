41aa48a9b4a121d0ff364ad5a3e86a0ae08be17c
jotoak <55738870+jotoak@users.noreply.github.com>
2021-02-18 19:45:51 +0100

    Kode for lab 3 endelig versjon

--- Lab 3/main.py
+++ Lab 3/main.py
@@ -1,19 +1,21 @@
+from numpy.lib.function_base import angle
 import ras_import
 from scipy import signal
 import numpy as np
 import matplotlib.pyplot as plt
 
-sample_periode, data=ras_import.raspi_import('radarData2.bin',2)
+sample_periode, data = ras_import.raspi_import('radarData.bin', 2)
 
 
-
-def doppler(f_d,f_0=24e9):
-    c=3e8
-    v=(c*f_d)/(2*f_0)
+def doppler(f_d, f_0=24e9):
+    c = 3e8
+    v = (c*f_d)/(2*f_0)
     return v
 
-def fart(v,f_0=24.13e9):
+
+def fart(v, f_0=24.13e9):
     return (2*f_0*v)/(3e8)
+
 
 def freq_from_fft(sig, fs):
     """
@@ -25,15 +27,19 @@ def freq_from_fft(sig, fs):
 
     # Find the peak and interpolate to get a more accurate peak
     i = np.argmax(abs(f))  # Just use this for less-accurate, naive version
-    #true_i = np.polyfit(np.log(abs(f)), i,4)[0]
+    # true_i = np.polyfit(np.log(abs(f)), i,4)[0] #The function that should be here is no longer supported
 
     # Convert to equivalent frequency
     return fs * i / len(windowed)
 
-def find_peak(fft,freqs):
-    i = np.argmax(fft)
+
+def find_peak(fft, freqs):
+    i = np.argmax(fft)  # Esay way to finding peek of fft
     peakFreq = freqs[i]
     return peakFreq
+
+
+# The bandpass filter is not neede but can be used if the signal contanis more noise
 '''
 def bandpass_filter(s, fs, high, low, order):
     om = fs/2
@@ -43,37 +49,39 @@ def bandpass_filter(s, fs, high, low, order):
 '''
 
 
-def complex_fft(data,Fs,real=0,imag=1):
-    #print(data)
-    ifi=data[:,real]
-    ifq=data[:,imag]
-    if(len(ifi)%2==1):
-        ifi=ifi[:-1]
-        ifq=ifq[:-1]
-    ifi=signal.detrend(ifi,axis=0)
-    ifq=signal.detrend(ifq,axis=0)
-    #print(ifi)
-    #print(ifq)
-    complex_signal=np.vectorize(complex)(ifi, ifq)
-    #print(complex_signal)
-    fft=np.fft.fft(complex_signal)
-    
-    #print(fft)
-    freq=np.fft.fftfreq(fft.size,1/Fs) #usikker på om dette er riktig
-    plt.plot(freq,fft)
-    plt.show()
-    return freq, fft
+def complex_fft(data, Fs, real=0, imag=1):
+    # print(data)
+    ifi = data[:, real]
+    ifq = data[:, imag]
+    if(len(ifi) % 2 == 1):
+        ifi = ifi[:-1]
+        ifq = ifq[:-1]
+    ifi = signal.detrend(ifi, axis=0)  # Remove the DC komponent
+    ifq = signal.detrend(ifq, axis=0)
+    # print(ifi)
+    # print(ifq)
+    complex_signal = np.vectorize(complex)(ifi, ifq)
+    # FIndin the diffrences in angle betwen to phasor to calculate wich way the dopler shift is
+    diff = np.rad2deg(
+        angle(complex_signal[100]))-np.rad2deg(angle(complex_signal[200]))
+    dir = int(diff)/abs(int(diff))
+    fft = np.fft.fft(complex_signal)
 
-def radar_speed(data,Fs=32.5e3):
-    freqs, fft=complex_fft(data,Fs)
-    #fft,freqs=bandPass(fft,freqs)
-    print(freqs)
-    print(fft)
-    peak_freq=find_peak(fft,freqs)
-    print(peak_freq)
-    speed=doppler(peak_freq)
+    # print(fft)
+    # Finds the frquency of the sampled fft
+    freq = np.fft.fftfreq(fft.size, 1/Fs)
+    plt.plot(freq, fft)
+    plt.show()
+    return freq, fft, dir
+
+
+def radar_speed(data, Fs=32.5e3):
+    freqs, fft, dir = complex_fft(data, Fs)
+    print("Retningen til farten", dir)
+    peak_freq = find_peak(fft, freqs)
+    # print(peak_freq)
+    speed = doppler(peak_freq)
     return speed
 
-print("Fart:",radar_speed(data))
 
-
+print("Fart:", radar_speed(data))
