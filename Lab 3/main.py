15f2b5470f334f58bd3be22125cfafd6855aec4c
jotoak <55738870+jotoak@users.noreply.github.com>
2021-02-18 17:01:50 +0100

    Kode som gir riktig fart, men rettning blir feil

--- Lab 3/main.py
+++ Lab 3/main.py
@@ -1,10 +1,9 @@
 import ras_import
 from scipy import signal
 import numpy as np
+import matplotlib.pyplot as plt
 
-
-file=""
-sample_periode, data=ras_import.raspi_import(file,2)
+sample_periode, data=ras_import.raspi_import('radarData2.bin',2)
 
 
 
@@ -13,41 +12,68 @@ def doppler(f_d,f_0=24e9):
     v=(c*f_d)/(2*f_0)
     return v
 
+def fart(v,f_0=24.13e9):
+    return (2*f_0*v)/(3e8)
+
 def freq_from_fft(sig, fs):
     """
     Estimate frequency from peak of FFT
     """
     # Compute Fourier transform of windowed signal
-    windowed = sig * np.blackmanharris(len(sig))
-    f = np.rfft(windowed)
+    windowed = sig * signal.blackmanharris(len(sig))
+    f = np.fft.fft(windowed)
 
     # Find the peak and interpolate to get a more accurate peak
     i = np.argmax(abs(f))  # Just use this for less-accurate, naive version
-    true_i = np.parabolic(np.log(abs(f)), i)[0]
+    #true_i = np.polyfit(np.log(abs(f)), i,4)[0]
 
     # Convert to equivalent frequency
-    return fs * true_i / len(windowed)
+    return fs * i / len(windowed)
 
-
+def find_peak(fft,freqs):
+    i = np.argmax(fft)
+    peakFreq = freqs[i]
+    return peakFreq
+'''
 def bandpass_filter(s, fs, high, low, order):
     om = fs/2
     b, a = signal.butter(order, [high / om, low / om], btype="band")
     a = signal.detrend(a, axis=0)   # Remove DC-component.
     return signal.lfilter(b, a, s)
+'''
+
 
 def complex_fft(data,Fs,real=0,imag=1):
+    #print(data)
     ifi=data[:,real]
     ifq=data[:,imag]
     if(len(ifi)%2==1):
         ifi=ifi[:-1]
         ifq=ifq[:-1]
-    compelx_signal=complex(ifi,ifq)
-    fft=np.fft.fft(compelx_signal)
-    freq=np.fft.fftfreq(fft.size) #usikker på om dette er riktig
+    ifi=signal.detrend(ifi,axis=0)
+    ifq=signal.detrend(ifq,axis=0)
+    #print(ifi)
+    #print(ifq)
+    complex_signal=np.vectorize(complex)(ifi, ifq)
+    #print(complex_signal)
+    fft=np.fft.fft(complex_signal)
+    
+    #print(fft)
+    freq=np.fft.fftfreq(fft.size,1/Fs) #usikker på om dette er riktig
+    plt.plot(freq,fft)
+    plt.show()
     return freq, fft
 
-def radar_speed(data,Fs):
-    fft, freqs=complex(data,Fs)
-    peak_freq=freq_from_fft(fft,Fs)
+def radar_speed(data,Fs=32.5e3):
+    freqs, fft=complex_fft(data,Fs)
+    #fft,freqs=bandPass(fft,freqs)
+    print(freqs)
+    print(fft)
+    peak_freq=find_peak(fft,freqs)
+    print(peak_freq)
     speed=doppler(peak_freq)
     return speed
+
+print("Fart:",radar_speed(data))
+
+
