608a893fc9c84b8c0900666656a569862d758120
Chris Vaksdal <chris.vaksdal@gmail.com>
2021-03-05 10:46:03 +0100

    added cardata -4 - 4

---
+++ Lab 3/sampleradar.sh
@@ -0,0 +1,5 @@
+/home/pi/src/adc/adc_sampler 32500 /home/pi/src/adc/adcData.bin
+cp ~/src/adc/adcData.bin ~/src/ttt4280_lab/Lab\ 3/radarData3.bin
+git add /home/pi/ttt4280_lab/Lab\ 3/radarData3.bin
+git commit -m "New data."
+git push
