100490775b9392ca09831aadf56ce7a265d7aa6b
Chris Vaksdal <chris.vaksdal@gmail.com>
2021-03-18 11:33:24 +0100

    Fixed analyze.py
    Needs peak-detection.

--- Lab 4/analyze.py
+++ Lab 4/analyze.py
@@ -2,10 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal as signal
 import csv
+from scipy.interpolate import UnivariateSpline as spline
 
+def bandpass_filter(s, fs, high, low, order):
+    om = fs/2
+    b, a = signal.butter(order, [high / om, low / om], btype="band")
+    a = signal.detrend(a, axis=0)   # Remove DC-component.
+    return signal.lfilter(b, a, s)
+
+def shelf_filter(s, fs, high):
+    b, a = signal.butter(10, (high*2/fs), btype="high")
+    a = signal.detrend(a, axis=0)   # Remove DC-component.
+    return signal.lfilter(b, a, s)
+    return s
 
 # Import data from bin file
-filename="output"
+filename="output_finger.csv"
 
 with open(filename) as csvfile:
     csvreader=csv.reader(csvfile, delimiter=" ")
@@ -17,16 +29,19 @@ with open(filename) as csvfile:
 
     data = signal.detrend(data, axis=0)  # removes DC component for each channel
     #sample_period *= 1e-6  # change unit to micro seconds
-    sample_period = 24
+    sample_period = 1/40
+    data = bandpass_filter(data, 1/sample_period, 0.8, 3.5, 10)
 
     # Generate time axis
     num_of_samples = data.shape[0]  # returns shape of matrix
     t = np.linspace(start=0, stop=num_of_samples*sample_period, num=num_of_samples)
 
     # Generate frequency axis and take FFT
+    cs = spline(t, data[:,1])
+    data = cs(data)
     freq = np.fft.fftfreq(n=num_of_samples, d=sample_period)
     spectrum = np.fft.fft(data, axis=0)  # takes FFT of all channels
-
+    spectrum = shelf_filter(spectrum, 1/sample_period, 1)
 
     # Plot the results in two subplots
     # NOTICE: This lazily plots the entire matrixes. All the channels will be put into the same plots.
@@ -43,5 +58,6 @@ with open(filename) as csvfile:
     plt.ylabel("Power [dB]")
     plt.plot(freq, 20*np.log(np.abs(spectrum))) # get the power spectrum
 
+    plt.legend(["r", "g", "b"])
     plt.show()
     #plt.savefig("noise.png")
